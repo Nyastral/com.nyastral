5e198a18959226d9f1678f2c67c48bca98584a33
Konrad Kubczyk <konrad@kubczyk.com>
2021-11-20 22:07:47 +0100

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    'index.html',
+    './src/html/*.html',
+    './src/js/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
